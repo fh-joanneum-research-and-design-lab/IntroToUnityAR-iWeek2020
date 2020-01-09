378b640fca9decf0ae896b6ae3869b28b8e5b9a6
furmankl <41745013+furmankl@users.noreply.github.com>
2020-01-09 13:51:08 +0100

    object spawner added

---
+++ IntroToAR-iWeek2020/Assets/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject spawnObject;
+    private ARTapToPlaceObject ARTapToPlaceObject;
+    void Start()
+    {
+        ARTapToPlaceObject = FindObjectOfType<ARTapToPlaceObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(spawnObject, ARTapToPlaceObject.transform.position, ARTapToPlaceObject.transform.rotation);
+        }
+    }
+}
